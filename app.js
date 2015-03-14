5567b130ec9f2764164b665bd203f86a4263c4ab
Mauro Pompilio <hackers.are.rockstars@gmail.com>
2015-03-14 14:45:48 +0000

    Save deps blobs

--- app.js
+++ app.js
@@ -3,18 +3,47 @@
 
 require("babel/register");
 
-var bodyParser  = require('body-parser');
-var express     = require('express');
-var app         = express();
+var PORT    = process.env.PORT || 5000;
+var BASEURL = process.env.HOST || 'http://localhost:' + PORT;
 
+var express       = require('express');
+var bodyParser    = require('body-parser');
+var serve_static  = require('serve-static');
+var octonode      = require('octonode');
+var redis         = require('./lib/redis');
+var debug         = require('debug')('librarian');
+var webhooks      = require('./lib/webhooks');
+var prStatus      = require('./lib/pr-status');
+var OAuth         = require('./lib/oauth');
+
+var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'jade');
+app.use(serve_static('static'));
 
-var webhook = require('./lib/webhook');
+var gh = new OAuth({
+  client:   process.env.GHCLIENT,
+  secret:   process.env.GHSECRET,
+  redirect: BASEURL + '/github/callback',
+  scope:    'repo:status'
+});
 
-app.post('/webhook', webhook);
+app.get('/github/login',    gh.login.bind(gh));
+app.get('/github/callback', gh.callback.bind(gh), function(req, res) {
+  var client = octonode.client(req.token);
+  client.me().info(function(err, me) {
+    redis.set(me.login, req.token)
+    .then(function() {
+      var url = BASEURL + '/webhooks/' + me.login;
+      res.render('welcome', {username: me.login, url: url});
+    });
+  });
+});
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log('Listening on', port);
+app.post('/webhooks/:username', webhooks);
+app.get('/repos/:owner/:repo/pull/:pr', prStatus);
+
+app.listen(PORT, function() {
+  debug('Listening on', PORT);
 });
