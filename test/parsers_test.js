71928562289e5eb8d3b8621eacf43005c5db0d11
Mauro Pompilio <hackers.are.rockstars@gmail.com>
2015-03-18 23:40:00 +0000

    Kids, don't write tests at lunch

--- test/parsers_test.js
+++ test/parsers_test.js
@@ -7,12 +7,12 @@ describe('Parser', function(){
   it('should parse package.json', function() {
     var str = fs.readFileSync(__dirname + '/fixtures/package.json').toString();
     var deps = parsers.npm(str);
-    assert(['babel', '^4.6.6.'], deps[0]);
+    assert.deepEqual(deps[0], ['babel', '^4.6.6']);
   });
 
   it('should parse Gemfile (naive)', function() {
     var str = fs.readFileSync(__dirname + '/fixtures/Gemfile').toString();
     var deps = parsers.rubygems(str);
-    assert(['oj', 'latest'], deps[0]);
+    assert.deepEqual(deps[0], ['oj', 'latest']);
   });
 });
