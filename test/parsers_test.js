c15113a2177a3d91975ec7d51cd042e171498575
Mauro Pompilio <hackers.are.rockstars@gmail.com>
2015-03-18 13:33:24 +0000

    Add some tests for the parser

---
+++ test/parsers_test.js
@@ -0,0 +1,18 @@
+var assert  = require('assert');
+var fs = require('fs');
+
+var parsers = require('../lib/parsers');
+
+describe('Parser', function(){
+  it('should parse package.json', function() {
+    var str = fs.readFileSync(__dirname + '/fixtures/package.json').toString();
+    var deps = parsers.npm(str);
+    assert(['babel', '^4.6.6.'], deps[0]);
+  });
+
+  it('should parse Gemfile (naive)', function() {
+    var str = fs.readFileSync(__dirname + '/fixtures/Gemfile').toString();
+    var deps = parsers.rubygems(str);
+    assert(['oj', 'latest'], deps[0]);
+  });
+});
