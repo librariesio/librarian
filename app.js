a8006708aa69fa8e0e4d224b2b2417b88d18518e
Mauro Pompilio <hackers.are.rockstars@gmail.com>
2015-03-18 01:38:52 +0000

    ETOOMANYCHANGES

--- app.js
+++ app.js
@@ -1,52 +1,41 @@
-/*jshint globalstrict:true, trailing:false, unused:true, node:true */
-"use strict";
+/*jshint strict:true, trailing:false, unused:true, node:true */
+'use strict';
 
 require("babel/register");
 
-var PORT    = process.env.PORT || 5000;
-var BASEURL = process.env.HOST || 'http://localhost:' + PORT;
-
-var express       = require('express');
-var bodyParser    = require('body-parser');
-var serve_static  = require('serve-static');
-var octonode      = require('octonode');
-var redis         = require('./lib/redis');
-var debug         = require('debug')('librarian');
-var webhooks      = require('./lib/webhooks');
-var prStatus      = require('./lib/pr-status');
-var home          = require('./lib/home');
-var OAuth         = require('./lib/oauth');
+var express      = require('express');
+var bodyParser   = require('body-parser');
+var serve_static = require('serve-static');
+var session      = require('express-session');
+var debug        = require('debug')('librarian');
+var router       = require('./lib/router');
+var ghauth       = require('./lib/oauth');
 
 var app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ 
+  extended: true 
+}));
 app.set('view engine', 'jade');
 app.use(serve_static('static'));
+app.use(session({
+  secret: 'teprefieroigualinternacional',
+  resave: false,
+  saveUninitialized: false
+}));
 
-var gh = new OAuth({
-  client:   process.env.GHCLIENT,
-  secret:   process.env.GHSECRET,
-  redirect: BASEURL + '/github/callback',
-  scope:    'repo:status'
-});
+// GitHub OAuth
+app.use('/github', ghauth);
 
-app.get('/github/login',    gh.login.bind(gh));
-app.get('/github/callback', gh.callback.bind(gh), function(req, res) {
-  var client = octonode.client(req.token);
-  client.me().info(function(err, me) {
-    redis.set(me.login, req.token)
-    .then(function() {
-      var url = BASEURL + '/webhooks/' + me.login;
-      res.render('welcome', {username: me.login, url: url});
-    });
-  });
-});
+app.get('/', router.index);
 
-app.get('/', home);
+app.get('/repos', router.repos);
+app.get('/repos/:owner/:repo/pull/:pr', router.prStatus);
 
-app.post('/webhooks/:username', webhooks);
-app.get('/repos/:owner/:repo/pull/:pr', prStatus);
+app.post('/webhooks/:username', router.webhooks);
+app.post('/webhooks', router.createWebhook);
 
-app.listen(PORT, function() {
-  debug('Listening on', PORT);
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  debug('Listening on', port);
 });
