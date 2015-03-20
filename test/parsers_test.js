33f073f08db1a1b4def99da56db152c683575354
Andrew Nesbitt <andrewnez@gmail.com>
2015-03-20 22:16:47 +0000

    Added packagist parser
    File is composer.json, very similar to npm/bower

--- test/parsers_test.js
+++ test/parsers_test.js
@@ -10,6 +10,12 @@ describe('Parser', function(){
     assert(['babel', '^4.6.6.'], deps[0]);
   });
 
+  it('should parse composer.json', function() {
+    var str = fs.readFileSync(__dirname + '/fixtures/composer.json').toString();
+    var deps = parsers.packagist(str);
+    assert(["laravel/framework", "5.0.*"], deps[0]);
+  });
+
   it('should parse Gemfile (naive)', function() {
     var str = fs.readFileSync(__dirname + '/fixtures/Gemfile').toString();
     var deps = parsers.rubygems(str);
