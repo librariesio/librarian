8c4cd19c8be7bd86ec53e56a987b4d25b8f8447b
Mauro Pompilio <hackers.are.rockstars@gmail.com>
2015-03-24 00:22:49 +0000

    Refactor Repo + tests

--- test/repo_test.js
+++ test/repo_test.js
@@ -9,12 +9,14 @@ var Repo = require('../lib/repo');
 var sinon = require('sinon');
 var payloads = require('./fixtures/github-payloads');
 
-
 describe('Repo class', function(){
-  this.timeout(5*1000);
 
-  var gh;
+  var gh, hooksManifest;
+
   beforeEach(function() {
+
+    // Stub GH API
+
     gh = new GitHub();
 
     var get = sinon.stub();
@@ -25,54 +27,74 @@ describe('Repo class', function(){
     get.withArgs('/repos/malditogeek/hooks/git/blobs/5dc0f3906430d87bfe001089e2280b9ee4ac24c5')
     .returns(Promise.resolve(payloads.blob));  
 
-    var stub = sinon.stub(gh, 'get', get);
+    sinon.stub(gh, 'get', get);
+
+    // Sample repo manifest
+
+    hooksManifest = [{
+      name: 'npm',
+      path: 'package.json',
+      sha: '5dc0f3906430d87bfe001089e2280b9ee4ac24c5'
+    }];
+
   });
 
-  it('should fetch manifests', function(done) {
+  it('should fetch master HEAD', function(done) {
     var repo = new Repo('malditogeek', 'hooks', gh);
-    return repo.findManifests()
-    .then(function(manifests) {
-      var expected = { 
-        npm: {
-          path: 'package.json',
-          mode: '100644',
-          type: 'blob',
-          sha: '5dc0f3906430d87bfe001089e2280b9ee4ac24c5',
-          size: 531,
-          url: 'https://api.github.com/repos/malditogeek/hooks/git/blobs/5dc0f3906430d87bfe001089e2280b9ee4ac24c5'
-        } 
-      };
-
-      assert.deepEqual(manifests, expected);
+    return repo.fetchMaster()
+    .then(function(sha) {
+      assert.equal(sha, '9611b058b6aaa0481eb77d504f9141f06e9b52ea', "Diff sha");
     })
     .then(done)
     .catch(done);
   });
 
-  it('should find dependencies foo', function(done) {
+  it('should find manifests at a given SHA', function(done) {
     var repo = new Repo('malditogeek', 'hooks', gh);
-    return repo.findManifests()
+    return repo.findManifestsAt('9611b058b6aaa0481eb77d504f9141f06e9b52ea')
     .then(function(manifests) {
-      return repo.fetchDependencies(manifests);
-    })
-    .then(function(dependencies) {
-      var expected = {npm: { octonode: '^0.6.15', 'node-gitter': '^1.2.7' }};
-      assert.deepEqual(dependencies, expected);
+      assert.deepEqual(manifests, hooksManifest, "Manifests don't match");
     })
     .then(done)
     .catch(done);
   });
 
-  it('should find dependencies 2', function(done) {
+  it('should fetch blobs given a manifest', function(done) {
+    hooksManifest[0].blob = payloads.blob;
+
+    var repo = new Repo('malditogeek', 'hooks', gh);
+    return repo.fetchBlobs(hooksManifest)
+    .then(function(blobs) {
+      assert.deepEqual(blobs, hooksManifest, "Blobs don't match");
+    })
+    .then(done)
+    .catch(done);
+  });
+
+  it('should be able to parse blobs', function(done) {
+    hooksManifest[0].blob = payloads.blob;
+    hooksManifest[0].deps = {octonode: '^0.6.15', 'node-gitter': '^1.2.7'};
+
+    var repo = new Repo('malditogeek', 'hooks', gh);
+    return repo.parseBlobs(hooksManifest)
+    .then(function(deps) {
+      delete hooksManifest.blob;
+      assert.deepEqual(deps, hooksManifest, "Deps don't match");
+    })
+    .then(done)
+    .catch(done);
+  });
+
+  it('should find dependencies', function(done) {
+    hooksManifest[0].deps = {octonode: '^0.6.15', 'node-gitter': '^1.2.7'};
+
     var repo = new Repo('malditogeek', 'hooks', gh);
     return repo.findDependencies()
     .then(function(dependencies) {
-      var expected = {npm: { octonode: '^0.6.15', 'node-gitter': '^1.2.7' }};
-      assert.deepEqual(dependencies, expected);
+      assert.deepEqual(dependencies, hooksManifest, "Deps don't match");
     })
     .then(done)
     .catch(done);
   });
-
 
 });
