a72c03339196f271cc2cc378ff7abd58e9fbc357
Mauro Pompilio <hackers.are.rockstars@gmail.com>
2015-05-12 23:28:16 +0100

    Add filter function to find manifest based on filepath and not just filename + other conditions if necessary

--- test/parsers_test.js
+++ test/parsers_test.js
@@ -9,78 +9,105 @@ var parsers = require('../lib/parsers');
 var platformTests = [
   {
     platform: 'npm',
-    manifest: 'package.json',
-    expected: [['babel', '^4.6.6']]
+    fixture: 'package.json',
+    expected: [['babel', '^4.6.6']],
+    validManifestPaths: ['package.json'],
+    invalidManifestPaths: ['node_modules/foo/package.json']
   },
   {
     platform: 'packagist',
-    manifest: 'composer.json',
-    expected: [["laravel/framework", "5.0.*"]]
+    fixture: 'composer.json',
+    expected: [["laravel/framework", "5.0.*"]],
+    validManifestPaths: ['composer.json'],
+    invalidManifestPaths: []
   },
   {
     platform: 'packagist',
-    manifest: 'composer2.json',
-    expected: [[]]
+    fixture: 'composer2.json',
+    expected: [[]],
+    validManifestPaths: [],
+    invalidManifestPaths: []
   },
   {
     platform: 'cargo',
-    manifest: 'Cargo.toml',
-    expected: [["rustc-serialize", "*"]]
+    fixture: 'Cargo.toml',
+    expected: [["rustc-serialize", "*"]],
+    validManifestPaths: ['Cargo.toml'],
+    invalidManifestPaths: []
   },
   {
     platform: 'elm',
-    manifest: 'elm-package.json',
-    expected: [["evancz/elm-markdown", "1.1.0 <= v < 2.0.0"]]
+    fixture: 'elm-package.json',
+    expected: [["evancz/elm-markdown", "1.1.0 <= v < 2.0.0"]],
+    validManifestPaths: ['elm-package.json'],
+    invalidManifestPaths: ['node_modules/foo/elm-package.json']
   },
   {
     platform: 'bower',
-    manifest: 'bower.json',
-    expected: [['jquery', '>= 1.9.1']]
+    fixture: 'bower.json',
+    expected: [['jquery', '>= 1.9.1']],
+    validManifestPaths: ['bower.json'],
+    invalidManifestPaths: ['node_modules/foo/bower.json']
   },
   {
     platform: 'pub',
-    manifest: 'pubspec.yaml',
-    expected: [['analyzer', '>=0.22.0 <0.25.0']]
+    fixture: 'pubspec.yaml',
+    expected: [['analyzer', '>=0.22.0 <0.25.0']],
+    validManifestPaths: ['pubspec.yaml'],
+    invalidManifestPaths: []
+
   },
   {
     platform: 'dub',
-    manifest: 'dub.json',
-    expected: [['vibe-d', '~>0.7.22']]
+    fixture: 'dub.json',
+    expected: [['vibe-d', '~>0.7.22']],
+    validManifestPaths: ['dub.json'],
+    invalidManifestPaths: []
   },
   {
     platform: 'rubygems',
-    manifest: 'Gemfile',
+    fixture: 'Gemfile',
     expected: [
       ['oj', '>= 0'],
       ['rails', '4.2.0'],
       ['leveldb-ruby', '0.15'],
       ['spring', '>= 0']
-    ]
+    ],
+    validManifestPaths: ['Gemfile'],
+    invalidManifestPaths: ['bundle/foo/Gemfile']
   },
   {
     platform: 'rubygems',
-    manifest: 'gems.rb',
+    fixture: 'gems.rb',
     expected: [
       ['oj', '>= 0'],
       ['rails', '4.2.0'],
       ['leveldb-ruby', '0.15'],
       ['spring', '>= 0']
-    ]
+    ],
+    validManifestPaths: ['gems.rb'],
+    invalidManifestPaths: []
   },
   {
     platform: 'cocoapods',
-    manifest: 'Podfile',
-    expected: [['AFNetworking', '~> 1.0']]
+    fixture: 'Podfile',
+    expected: [['AFNetworking', '~> 1.0']],
+    validManifestPaths: ['Podfile'],
+    invalidManifestPaths: []
   },
   {
     platform: 'dpkg',
-    manifest: 'dpkg',
-    expected: [['accountsservice', '0.6.15-2ubuntu9.6']]
+    fixture: 'dpkg',
+    expected: [['accountsservice', '0.6.15-2ubuntu9.6']],
+    validManifestPaths: ['dpkg'],
+    invalidManifestPaths: []
   },
   {
     platform: 'hex',
-    manifest: 'mix.exs',
-    expected: [['poison', '~> 1.3.1']]
+    fixture: 'mix.exs',
+    expected: [['poison', '~> 1.3.1']],
+    validManifestPaths: ['mix.exs'],
+    invalidManifestPaths: []
   }
 ];
 
@@ -88,8 +115,8 @@ describe('Parser', function(){
   this.timeout(5000);
 
   platformTests.forEach(function(test) {
-    it('should handle '+ test.platform +' manifests', function(done) {
-      var str = fs.readFileSync(__dirname + '/fixtures/' + test.manifest).toString();
+    it('should handle '+ test.platform +' fixtures', function(done) {
+      var str = fs.readFileSync(__dirname +'/fixtures/'+ test.fixture).toString();
       parsers.parse(test.platform, str)
       .then(function(packages) {
         test.expected.forEach(function(pkg, i) {
@@ -99,5 +126,20 @@ describe('Parser', function(){
       .then(done)
       .catch(done);
     });
+
+    it('should match valid '+ test.platform +' manifest paths', function() {
+      test.validManifestPaths.forEach(function(path) {
+        var platform = parsers.findPlatform(path);
+        assert.equal(test.platform, platform);
+      });
+    });
+
+    it('should ignore invalid '+ test.platform +' manifest paths', function() {
+      test.invalidManifestPaths.forEach(function(path) {
+        var platform = parsers.findPlatform(path);
+        assert.equal(platform, null);
+      });
+    });
   });
+
 });
