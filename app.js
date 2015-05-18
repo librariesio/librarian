4a344d020adaca870f88f8d7dc04ef533b63a3b6
Mauro Pompilio <hackers.are.rockstars@gmail.com>
2015-05-18 21:25:10 +0100

    Handle error properly with Bugsnag

--- app.js
+++ app.js
@@ -11,40 +11,30 @@ var multer       = require('multer');
 var bugsnag      = require('bugsnag');
 var debug        = require('debug')('librarian');
 var router       = require('./lib/router');
-var ghauth       = require('./lib/oauth');
 
 bugsnag.register("b9d5d0c5b9ecdcf14731645900d4f5be");
 
 var app = express();
 app.use(bugsnag.requestHandler);
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'jade');
 app.use(serve_static('static'));
-app.use(session({
-  secret: 'teprefieroigualinternacional'
-}));
-app.use(bugsnag.errorHandler);
+app.use(session({secret: 'teprefieroigualinternacional'}));
 app.disable('x-powered-by');
 
-var authed = function(req, res, next) {
-  return req.session.user ? next() : res.redirect('/');
-};
-
-// GitHub OAuth
-//app.use('/github', ghauth);
-
 app.get('/', router.index);
-
 app.get('/repos/:owner/:repo', router.repoInfo);
 app.get('/repos/:owner/:repo/pull/:pr', router.prStatus);
-
 app.post('/webhooks/:username', router.webhooks);
 app.post('/webhooks', router.createWebhook);
-app.post('/subscribe', router.subscribe);
 app.post('/manifests', [multer({dest: './uploads/'}), router.parseManifests]);
+
+app.use(bugsnag.errorHandler);
+app.use(function(err, req, res, next) {
+  console.error('ERR', err);
+  res.status(500).send({error: 'Something went wrong.'});
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
