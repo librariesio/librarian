5e3d54355056e2a0a59c5ec0ddeab252b512aaa7
Mauro Pompilio <hackers.are.rockstars@gmail.com>
2015-06-01 23:50:15 +0100

    Reduce memory footprint, GC collect periodically

--- app.js
+++ app.js
@@ -1,61 +1,52 @@
-/*jshint strict:true, trailing:false, unused:true, node:true */
 'use strict';
 
 require("babel/register");
 
-var cluster = require('cluster');
-var numCPUs = require('os').cpus().length;
-var workers = process.env.WEB_CONCURRENCY;
+var express      = require('express');
+var bodyParser   = require('body-parser');
+var serve_static = require('serve-static');
+var session      = require('cookie-session');
+var memwatch     = require('memwatch-next');
+var bugsnag      = require('bugsnag');
+var debug        = require('debug')('librarian');
+var router       = require('./lib/router');
 
-console.log('CPUs:', numCPUs);
-console.log('workers:', workers);
+bugsnag.register("b9d5d0c5b9ecdcf14731645900d4f5be");
 
-if (cluster.isMaster) {
-  // Fork workers.
-  for (var i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+var app = express();
+app.use(bugsnag.requestHandler);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'jade');
+app.use(serve_static('static'));
+app.use(session({secret: 'teprefieroigualinternacional'}));
+app.disable('x-powered-by');
 
-  cluster.on('exit', function(worker, code, signal) {
-    console.log('worker ' + worker.process.pid + ' died');
-    cluster.fork();
-  });
+app.get('/', router.index);
+app.get('/repos/:owner/:repo', router.repoInfo);
 
-} else {
+// File uploads parser
+//var multer = require('multer');
+//app.post('/manifests', [multer({dest: './uploads/'}), router.parseManifests]);
 
-  var express      = require('express');
-  var bodyParser   = require('body-parser');
-  var serve_static = require('serve-static');
-  var session      = require('cookie-session');
-  var multer       = require('multer');
-  var bugsnag      = require('bugsnag');
-  var debug        = require('debug')('librarian');
-  var router       = require('./lib/router');
-  
-  bugsnag.register("b9d5d0c5b9ecdcf14731645900d4f5be");
-  
-  var app = express();
-  app.use(bugsnag.requestHandler);
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({extended: true}));
-  app.set('view engine', 'jade');
-  app.use(serve_static('static'));
-  app.use(session({secret: 'teprefieroigualinternacional'}));
-  app.disable('x-powered-by');
-  
-  app.get('/', router.index);
-  app.get('/repos/:owner/:repo', router.repoInfo);
-  app.post('/manifests', [multer({dest: './uploads/'}), router.parseManifests]);
-  
-  app.use(bugsnag.errorHandler);
-  app.use(function(err, req, res, next) {
-    console.error('ERR', err);
-    res.status(500).send({error: 'Something went wrong.'});
+app.use(bugsnag.errorHandler);
+app.use(function(err, req, res, next) {
+  console.error('ERR', err);
+  res.status(500).send({error: 'Something went wrong.'});
+});
+
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log('Listening on', port);
+
+  memwatch.on('leak', function(info) { 
+    console.log('leak:', info);
   });
   
-  var port = process.env.PORT || 5000;
-  app.listen(port, function() {
-    console.log('Listening on', port);
+  memwatch.on('stats', function(stats) {
+    console.log('stats:', stats);
   });
-
-}
+  
+  setInterval(memwatch.gc, 60000);
+});
